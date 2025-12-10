9358e14e9f5f9545ac813e9278deceb75295f441
Redwanul-haque-1 <129584773+Redwanul-haque-1@users.noreply.github.com>
2025-12-10 13:21:30 +0600

    Delete PHP directory

--- PHP/Exercise_2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$amount = 500;
-$vat = 0.15 * $amount ;
-echo "The amount is $amount";
-echo "<br>";
-echo "15% VAT is $vat";
-
-?>
--- PHP/Exercise_3_If_else.php
+++
@@ -1,8 +0,0 @@
-<?php
-$a = 93;
-if ($a%2 == 0)
-   echo "The number is even";
-else 
-    echo "The number is odd";
-?>
-
